e9c7631a4876c5357217335789078fb20c215a32
Sergio Carracedo <info@sergiocarracedo.es>
2022-05-31 22:33:17 +0200

    chore: remove unused pluings

--- plugins/tabs.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import { Tab, Tabs } from 'vue3-tabs-component';
-
-export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp
-    .component('tabs', Tabs)
-    .component('tab', Tab)
-})
